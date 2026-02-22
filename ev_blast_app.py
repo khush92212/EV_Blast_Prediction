43979ee4ce6d4996a6d59380827ff0f0702020d8
Suman <aaltufaltu5125@gmail.com>
2026-02-22 14:41:11 +0530

    Update ev_blast_app.py

--- ev_blast_app.py
+++ ev_blast_app.py
@@ -1,43 +1,38 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import joblib
 
 model = joblib.load("dtc_ev_model.pkl")
-label_encoder = joblib.load("label_encoder_ev.pkl")
+encoder = joblib.load("label_encoder_ev.pkl")
 
-st.title("EV Blast Prediction")
+st.title("EV_Blast_prediction !")
 
-options = ["Yes", "No"]
+Battery_Type = st.selectbox("Battery_Type")
+Poor_Cell_Design = st.selectbox("Poor_Cell_Design")
+External_Abuse = st.selectbox("External_Abuse")
+Poor_Battery_Design = st.selectbox("Poor_Battery_Design")
+Short_Circuits = st.selectbox("Short_Circuits")
+Temperature = st.selectbox("Temperature")
+Overcharge_Overdischarge =  st.selectbox("Overcharge_Overdischarge")
+Battery_Maintenance = st.selectbox("Battery_Maintenance")
+Battery_Health = st.selectbox("Battery_Health")
 
-Battery_Type = st.selectbox("Battery Type", options)
-Poor_Cell_Design = st.selectbox("Poor Cell Design", options)
-External_Abuse = st.selectbox("External Abuse", options)
-Poor_Battery_Design = st.selectbox("Poor Battery Design", options)
-Short_Circuits = st.selectbox("Short Circuits", options)
-Temperature = st.selectbox("High Temperature", options)
-Overcharge_Overdischarge = st.selectbox("Overcharge/Overdischarge", options)
-Battery_Maintenance = st.selectbox("Battery Maintenance", options)
-Battery_Health = st.selectbox("Battery Health", options)
-
-# Create dataframe
 input_data = pd.DataFrame({
-    "Battery_Type": [Battery_Type],
-    "Poor_Cell_Design": [Poor_Cell_Design],
-    "External_Abuse": [External_Abuse],
-    "Poor_Battery_Design": [Poor_Battery_Design],
-    "Short_Circuits": [Short_Circuits],  # fixed error here
-    "Temperature": [Temperature],
-    "Overcharge_Overdischarge": [Overcharge_Overdischarge],
-    "Battery_Maintenance": [Battery_Maintenance],
-    "Battery_Health": [Battery_Health]
+    "Battery_Type" : [Battery_Type],
+    "Poor_Cell_Design" : [Poor_Cell_Design],
+    "External_Abuse" : [External_Abuse],
+    "Poor_Battery_Design" : [Poor_Battery_Design],
+    "Short_Circuits" : [= Short_Circuits],
+    "Temperature" : [Temperature],
+    "Overcharge_Overdischarge" : [Overcharge_Overdischarge],
+    "Battery_Maintenance" : [Battery_Maintenance],
+    "Battery_Health" : [Battery_Health]
 })
 
-input_data = input_data.apply(lambda x: label_encoder.transform(x))
-
-if st.button("Predict"):
-    prediction = model.predict(input_data)[0]
-
-    if prediction == 1:   # assuming 1 = Blast
-        st.error("⚠️ High Risk: Blast")
-    else:
-        st.success("✅ Moderate / Safe Condition")
+if st.button("predict"):
+  prediction = model.predict(input_data)[0]
+  if prediction == "Blast":
+    st.success("Blast")
+  else:
+    st.error("Moderate")
