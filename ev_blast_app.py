479d9af4e25cfef2abc5277fec5f035b145b8f78
Suman <aaltufaltu5125@gmail.com>
2026-02-22 14:45:27 +0530

    Update ev_blast_app.py

--- ev_blast_app.py
+++ ev_blast_app.py
@@ -10,14 +10,14 @@ st.title("EV_Blast_prediction !")
 options = ["Yes", "No"]
 
 Battery_Type = st.selectbox("Battery_Type", options)
-Poor_Cell_Design = st.selectbox("Poor_Cell_Design")
-External_Abuse = st.selectbox("External_Abuse")
-Poor_Battery_Design = st.selectbox("Poor_Battery_Design")
-Short_Circuits = st.selectbox("Short_Circuits")
-Temperature = st.selectbox("Temperature")
-Overcharge_Overdischarge =  st.selectbox("Overcharge_Overdischarge")
-Battery_Maintenance = st.selectbox("Battery_Maintenance")
-Battery_Health = st.selectbox("Battery_Health")
+Poor_Cell_Design = st.selectbox("Poor_Cell_Design", options)
+External_Abuse = st.selectbox("External_Abuse", options)
+Poor_Battery_Design = st.selectbox("Poor_Battery_Design", options)
+Short_Circuits = st.selectbox("Short_Circuits", options)
+Temperature = st.selectbox("Temperature", options)
+Overcharge_Overdischarge =  st.selectbox("Overcharge_Overdischarge", options)
+Battery_Maintenance = st.selectbox("Battery_Maintenance", options)
+Battery_Health = st.selectbox("Battery_Health", options)
 
 input_data = pd.DataFrame({
     "Battery_Type" : [Battery_Type],
