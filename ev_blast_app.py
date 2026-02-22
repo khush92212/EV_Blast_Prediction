8eaae767399ceadca26ec207062eb13e565c2d21
Suman <aaltufaltu5125@gmail.com>
2026-02-22 16:19:58 +0530

    Update ev_blast_app.py

--- ev_blast_app.py
+++ ev_blast_app.py
@@ -4,7 +4,7 @@ import joblib
 
 # ---------------- LOAD MODEL ----------------
 model = joblib.load("dtc_ev_model.pkl")
-encoders = joblib.load("label_encoder_ev.pkl")
+encoders = joblib.load("label_encoder_ev(1).pkl")
 
 st.title("EV Blast Prediction App")
 
